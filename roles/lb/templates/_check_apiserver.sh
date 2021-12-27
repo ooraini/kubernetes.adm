eca2d92b5ad6dab72b144c7a46bf40a5bcf3d826
Omar Aloraini <ooraini.dev@gmail.com>
2021-12-27 10:27:01 +0300

    LB improvements
    New variable keepalived_scripts_path
    New Variable keepalived_peers
    Keepalvied unicast
    HAProxy cap logs to alert

--- roles/lb/templates/_check_apiserver.sh
+++ roles/lb/templates/_check_apiserver.sh
@@ -6,7 +6,7 @@ errorExit() {
 }
 
 curl --silent --max-time 2 --insecure https://localhost:{{ apiserver_lb_port }}/ -o /dev/null || errorExit "Error GET https://localhost:{{ apiserver_lb_port }}/"
-# TODO: IPV6 or SELinux policy
-if cat /proc/net/fib_trie | grep -q {{ keepalived_vip }}; then
+
+if ip ad | grep -q {{ keepalived_vip }}; then
     curl --silent --max-time 2 --insecure https://{{ keepalived_vip }}:{{ apiserver_lb_port }}/ -o /dev/null || errorExit "Error GET https://{{ keepalived_vip }}:{{ apiserver_lb_port }}/"
 fi
